c4a647c86be1c0bdb3fe62a3a5b942cfcb557cac
sammyshakes <sam.shakespeare@gmail.com>
2023-02-27 14:27:15 -0500

    .

---
+++ script/DeployL1.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+
+import "../src/Counter.sol";
+
+contract DeployL1 is Script {
+    // Deployments
+    Counter public counter;
+    
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        //Deploy Contracts
+        counter = new Counter();        
+
+        vm.stopBroadcast();
+    }
+}
--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+// import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
 contract Counter {
     uint256 public number;
 
